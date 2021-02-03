a714885fe528f71b58701af4506e6c8b21aeea53
HP <HP@LAPTOP-SMC8HIAI>
2021-02-03 08:49:41 +0530

    commit for testactitime

---
+++ testActiTime/src/test/java/actiTimeTest/ActiTimeTestValidLogin.java
@@ -0,0 +1,51 @@
+package actiTimeTest;
+
+import java.util.concurrent.TimeUnit;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+//author suraj
+public class ActiTimeTestValidLogin {
+	WebDriver driver;
+	String urlApp="http://laptop-smc8hiai/login.do";
+	
+	@BeforeClass
+	public void setUP()
+	{
+		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get(urlApp);
+		
+		
+	}
+	
+	
+	@Test
+	public void testActiTime()
+	{
+		
+		
+		String loginTitle = driver.getTitle();
+		Assert.assertEquals(loginTitle, "actiTIME - Login");
+		Reporter.log("The loginPage title is Verified",true);
+		
+	}
+	
+	
+	
+	@AfterClass
+	public void tearDown()
+	{
+		driver.quit();
+	}
+
+}
